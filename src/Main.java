1f20ade8a5aeb7012b3652ef36b4c29c367a94ef
Ирина Капкова <kapiral@rambler.ru>
2022-11-12 12:43:14 +0400

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        partRandomArrayElements(15, 100, 3, 7);
+    }
+
+    public static int[] partRandomArrayElements(int number, int max, int first, int last) {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[number];
+        System.out.println("Весь массив с нечетными элементами: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(max);
+            if (arr[i] % 2 == 0) {
+                arr[i] = arr[i] + 1;
+            }
+            System.out.print(arr[i] + ", ");
+        }
+        System.out.println();
+        System.out.println("Массив с " + first + " по " + last + " элемент массива:");
+        for (int i = first; i <= last; i++) {
+            System.out.print(arr[i] + ", ");
+        }
+        return arr;
     }
 }
